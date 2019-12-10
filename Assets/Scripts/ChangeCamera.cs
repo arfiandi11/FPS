4921153792515b3cbe388e50ded26cdd2285daa6
SausanNabila <58455532+SausanNabila@users.noreply.github.com>
2019-12-10 13:26:11 +0700

    scriptbarubangett

--- Assets/Scripts/ChangeCamera.cs
+++ Assets/Scripts/ChangeCamera.cs
@@ -8,33 +8,6 @@ public class ChangeCamera : MonoBehaviour
     public GameObject FPS;
     public GameObject TPS;
 
-<<<<<<< HEAD
-    // Update is called once per frame
-    void Update()
-    {
-        if (Input.GetButtonDown ("Camera")) 
-        {
-            if (CameraMode == 1) {
-                CameraMode = 0;
-            }
-            else {
-                CameraMode += 1;
-            }
-        StartCoroutine (SwitchCamera());
-        }
-    }
-
-    IEnumerator SwitchCamera () 
-    {
-        yield return new WaitForSeconds (0.01f);
-        if (CameraMode == 0) {
-            FPS.SetActive(true);
-            TPS.SetActive(false);
-        }
-        if (CameraMode == 1) {
-            FPS.SetActive (false);
-            TPS.SetActive (true);
-=======
     // Start is called before the first frame update
     void Start()
     {
@@ -64,7 +37,6 @@ public class ChangeCamera : MonoBehaviour
     if (CameraMode == 1) {
         FPS.SetActive (false);
         TPS.SetActive (true);
->>>>>>> d5f921440475e25c3c926765154506c735796b8f
     }
 }
 
--- Assets/Scripts/PlayerAnimation.cs
+++ Assets/Scripts/PlayerAnimation.cs
@@ -5,33 +5,19 @@ using UnityEngine;
 public class PlayerAnimation : MonoBehaviour
 {
     public float speed = 4;
-<<<<<<< HEAD
-	public float rotSpeed = 80;
-	public float rot = 0f;
-	public float gravity = 8;
-	Animator anim;
-	Vector3 moveDir = Vector3.zero;
-	CharacterController controller;
-=======
     public float rotSpeed = 80;
     public float rot = 0f;
     public float gravity = 8;
     Animator anim;
     Vector3 moveDir = Vector3.zero;
     CharacterController controller;
->>>>>>> d5f921440475e25c3c926765154506c735796b8f
 
     // Start is called before the first frame update
     void Start()
     {
-<<<<<<< HEAD
-        controller = GetComponent<CharacterController>();
-        anim = GetComponent<Animator>();
-=======
 
     controller = GetComponent<CharacterController>();
     anim = GetComponent<Animator>();
->>>>>>> d5f921440475e25c3c926765154506c735796b8f
 
     }
 
@@ -39,108 +25,6 @@ public class PlayerAnimation : MonoBehaviour
     void Update()
     {
         Movement();
-<<<<<<< HEAD
-	    GetInput();
-	    SwapAk47();
-	    SwapShotgun();
-
-    }
-
-    void Movement() 
-    {
-	    if (controller.isGrounded) 
-        {
-	        if (Input.GetKey(KeyCode.W)) 
-            {
-	            if(anim.GetBool("shot") == true)
-                {
-	                return;
-	            }
-	            else if (anim.GetBool("shot") == false) 
-                {
-	                anim.SetBool("run", true);
-	                anim.SetInteger("condition", 1);
-	                moveDir = new Vector3 (0, 0, 1);
-	                moveDir *= speed;
-	                moveDir = transform.TransformDirection(moveDir);
-	            }
-	        }
-	        if (Input.GetKeyUp(KeyCode.W)) 
-            {
-	            anim.SetBool("run", false);
-	            anim.SetInteger("condition", 0);
-	            moveDir = new Vector3 (0, 0, 0);
-	        }
-	        rot += Input.GetAxis("Horizontal") * rotSpeed * Time.deltaTime;
-	        transform.eulerAngles = new Vector3 (0, rot, 0);
-	        moveDir.y -= gravity * Time.deltaTime;
-	        controller.Move(moveDir * Time.deltaTime);
-	    }
-    }
-
-	void GetInput() 
-    {
-	    if (Input.GetButtonDown("Fire1")) 
-        {
-	        if (anim.GetBool("run") == true) 
-            {
-	            anim.SetBool("run", false);
-	            anim.SetInteger("condition", 0);
-	        }
-	        if (anim.GetBool("run") == false) 
-            {
-	            Attack();
-	        }
-	    }
-	}
-
-    IEnumerator AttackRoutine() 
-    {
-        anim.SetBool("shot", true);
-        anim.SetInteger("condition", 2);
-        yield return new WaitForSeconds(0.5f);
-        anim.SetInteger("condition", 0);
-        anim.SetBool("shot", false);
-    }
-
-    void Attack() 
-    {
-    StartCoroutine(AttackRoutine());
-    }
-
-	void SwapAk47() 
-    {
-	    if (Input.GetKey(KeyCode.Alpha1)) 
-        {
-	        anim.SetBool("switchWeapon", true);
-	        anim.SetInteger("condition", 3);
-	    }
-	    else if (Input.GetKeyUp(KeyCode.Alpha1)) 
-        {
-	        anim.SetBool("switchWeapon", false);
-	        anim.SetInteger("condition", 0);
-	    }
-	}
-	 
-	void SwapShotgun() 
-    {
-	    if (Input.GetKey(KeyCode.Alpha2)) 
-        {
-	        anim.SetBool("switchWeapon", true);
-	        anim.SetInteger("condition", 3);    
-	    }
-	    else if (Input.GetKeyUp(KeyCode.Alpha2)) 
-        {
-	        anim.SetBool("switchWeapon", false);
-	        anim.SetInteger("condition", 0);
-	    }
-	}
-
-
-
-
-
-=======
         GetInput();
         SwapAk47();
         SwapShotgun();
@@ -218,6 +102,5 @@ void SwapShotgun() {
         anim.SetInteger("condition", 0);
     }
 }
->>>>>>> d5f921440475e25c3c926765154506c735796b8f
 
 }
